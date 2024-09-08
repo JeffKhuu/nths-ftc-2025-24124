150821bbeacc3b547e26f8964fa32e75635017ae
Jeff Khuu <Jeffrey_Kh@outlook.com>
2024-09-08 11:29:39 -0600

    Updated FTC Dashboard to v0.4.15

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Main.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.DriveTrain;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.Slide;
