18308b35ff87b0b4becf5af774d04aaa7451e2e7
dark-arashi <emailtosehaj@gmail.com>
2024-11-30 00:08:47 -0700

    redRight Basic Structure (untested co-ordinates)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/instructions/RedRightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/instructions/RedRightAuto.java
@@ -34,7 +34,7 @@ public class RedRightAuto extends AutoInstructions {
         Actions.runBlocking(new SequentialAction(
                 claw.setTo(Claw.ClawState.CLOSED),
                 wrist.moveTo(Wrist.WristState.INACTIVE),
-
+                //1st Specimen Clip
                 // Hang Preloaded Specimen
                 new ParallelAction(
                         slides.moveTo(Slide.SlideState.HIGH_RUNG.position),
@@ -49,7 +49,7 @@ public class RedRightAuto extends AutoInstructions {
                 driveTrain.strafeTo(12, -50),
 
                 // TODO: Push all 3 specimen into the observation station
-
+                //2nd Specimen Clip
                 // Get Specimen from Observation Station
                 driveTrain.strafeTo(48, -50),
                 new ParallelAction(
@@ -63,11 +63,100 @@ public class RedRightAuto extends AutoInstructions {
                         driveTrain.strafeTo(8, -50),
                         slides.moveTo(Slide.SlideState.CLIP_HIGH_CHAMBER.position)
                 ),
-                driveTrain.turnTo(90),
                 driveTrain.strafeTo(8, -35),
+                driveTrain.turnTo(90),
                 new SleepAction(0.2),
                 slides.moveTo(Slide.SlideState.CLIP_HANG.position),
-                driveTrain.strafeTo(8, -50)
+                driveTrain.strafeTo(8, -50),
+
+                //Push 3 specimen into the observation station
+
+                //1st Sample Push (untested)
+                driveTrain.strafeTo(40,-36),
+                driveTrain.strafeTo(44,-36),
+                driveTrain.strafeTo(44,-50),
+                driveTrain.strafeTo(44,-60),
+
+                //2nd Sample Push (untested)
+
+                driveTrain.strafeTo(44,-36),
+                driveTrain.strafeTo(54,-36),
+                driveTrain.strafeTo(54,-50),
+                driveTrain.strafeTo(54,-65),
+
+                //3rd Sample Push (untested) (needs precis co-ordinates)
+
+                driveTrain.strafeTo(54,-36),
+                driveTrain.turnTo(0),
+                driveTrain.strafeTo(64,-50),
+                driveTrain.strafeTo(64,-60),
+                driveTrain.strafeTo(64,-65),
+                driveTrain.strafeTo(54,-65),
+
+                //3rd Specimen Clip
+
+                driveTrain.strafeTo(48, -50),
+                new ParallelAction(
+                        driveTrain.turnTo(270),
+                        slides.moveTo(Slide.SlideState.CLIPPER.position)
+                ),
+                driveTrain.strafeTo(48, -70),
+
+                // Hang Specimen using Static Clip
+
+                new ParallelAction(
+                        driveTrain.strafeTo(4, -50),
+                        slides.moveTo(Slide.SlideState.CLIP_HIGH_CHAMBER.position)
+                ),
+                driveTrain.turnTo(90),
+                driveTrain.strafeTo(4, -35),
+                new SleepAction(0.2),
+                slides.moveTo(Slide.SlideState.CLIP_HANG.position),
+                driveTrain.strafeTo(4, -50),
+
+                //4th Specimen Clip
+
+
+                driveTrain.strafeTo(48, -50),
+                new ParallelAction(
+                        driveTrain.turnTo(270),
+                        slides.moveTo(Slide.SlideState.CLIPPER.position)
+                ),
+                driveTrain.strafeTo(48, -70),
+
+                // Hang Specimen using Static Clip
+
+                new ParallelAction(
+                        driveTrain.strafeTo(6, -50),
+                        slides.moveTo(Slide.SlideState.CLIP_HIGH_CHAMBER.position)
+                ),
+                driveTrain.turnTo(90),
+                driveTrain.strafeTo(6, -35),
+                new SleepAction(0.2),
+                slides.moveTo(Slide.SlideState.CLIP_HANG.position),
+                driveTrain.strafeTo(6, -50),
+
+                //5th Specimen Clip
+
+                driveTrain.strafeTo(48, -50),
+                new ParallelAction(
+                        driveTrain.turnTo(270),
+                        slides.moveTo(Slide.SlideState.CLIPPER.position)
+                ),
+                driveTrain.strafeTo(48, -70),
+
+                // Hang Specimen using Static Clip
+
+                new ParallelAction(
+                        driveTrain.strafeTo(11, -50),
+                        slides.moveTo(Slide.SlideState.CLIP_HIGH_CHAMBER.position)
+                ),
+                driveTrain.turnTo(90),
+                driveTrain.strafeTo(11, -35),
+                new SleepAction(0.2),
+                slides.moveTo(Slide.SlideState.CLIP_HANG.position),
+                driveTrain.strafeTo(11, -50)
+
         ));
     }
 
